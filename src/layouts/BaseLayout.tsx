81190f7fe1ec2f8b1a7c5d85fc2d730dd59e7d45
Lâm Thiên Bảo <lamthienbao0106@gmail.com>
2024-05-15 15:18:38 +0700

    first setup (navbar, routing)

---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+export interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+const BaseLayout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Navbar />
+      <main className="px-32">{children}</main>
+    </>
+  );
+};
+
+export default BaseLayout;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      theme: {
+        colors: {
+          mainColor: "#42484E",
+          secondColor: "#e89b26",
+          secondColorBrighter: "#ecaf51",
+          thirdColor: "#FFECCC",
+          bgColor: "#171d22",
+          detail: "#193445",
+          ongoing: "#007C4E",
+          fistAnime: "#8789C0",
+          secondAnime: "#5DFDCB",
+          thirdAnime: "#7cc6fe",
+          other: "#c2cae8",
+          opacityText: "#22282d",
+
+          outColor: "#e89b26",
+        },
+
+        fontFamily: {
+          mainFont: "Nunito, Arial, sans-serif",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
